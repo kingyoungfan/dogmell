7778866eab14089f81723347063cd7294b6e0ddf
yangyang <youngqiankun@163.com>
2017-09-01 17:14:26 +0800

    udpate

--- src/main/java/com/yytec/dogmell/controller/UserController.java
+++ src/main/java/com/yytec/dogmell/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.yytec.dogmell.controller;
 
+import com.yytec.dogmell.models.Permissions;
+import com.yytec.dogmell.models.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -13,6 +15,22 @@ import java.util.Map;
 @RestController
 @RequestMapping("api")
 public class UserController {
+
+    @GetMapping("user")
+    public Map<String, Object> user() {
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("success", true);
+        User user = new User();
+        user.setId(1);
+        user.setUsername("guest");
+        Permissions permissions = new Permissions();
+        permissions.setRole("guest");
+        String[] visit = {"1", "2", "21", "7", "5", "51", "52", "53"};
+        permissions.setVisit(visit);
+        user.setPermissions(permissions);
+        ret.put("user", user);
+        return ret;
+    }
 
     @PostMapping("/user/login")
     public Map<String, Object> login(@RequestBody String body) {
