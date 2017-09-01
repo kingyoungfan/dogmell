9eafea2275ef37caa65d9473018e6beffc126015
yangyang <youngqiankun@163.com>
2017-09-01 18:38:20 +0800

    udpate

--- src/main/java/com/yytec/dogmell/controller/IndexController.java
+++ src/main/java/com/yytec/dogmell/controller/IndexController.java
@@ -13,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index() {
         return "李影影是个小狗狗";
+    }
+
+    @GetMapping("weather/now.json")
+
+    public String now() {
+        return "{\"results\":[{\"location\":{\"id\":\"WS10730EM8EV\",\"name\":\"深圳\",\"country\":\"CN\",\"path\":\"深圳,深圳,广东,中国\",\"timezone\":\"Asia/Shanghai\",\"timezone_offset\":\"+08:00\"},\"now\":{\"text\":\"多云\",\"code\":\"4\",\"temperature\":\"31\"},\"last_update\":\"2017-09-01T17:20:00+08:00\"}]}";
     }
 }
