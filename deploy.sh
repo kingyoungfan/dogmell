b3500fcaebcbdb5805eb34f58a117d1a757a6949
dogmell <server@dogmell.com>
2017-09-23 22:27:19 +0800

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+echo '******git pull start******'
+git pull
+echo '******git pull end********'
+sh /home/dogmell/jetty/bin/jetty.sh stop
 
 mvn clean package -Dmaven.test.skip=true
 
 rm /home/dogmell/jetty/webapps/root.war
 
 cp target/root.war /home/dogmell/jetty/webapps
+
+sh /home/dogmell/jetty/bin/jetty.sh start
