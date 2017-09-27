4e4275441e07e548eea745373027ac19efd3a12b
yangyang <youngqiankun@163.com>
2017-09-27 11:55:28 +0800

    udapte

--- src/main/java/com/yytec/dogmell/controller/IndexController.java
+++ src/main/java/com/yytec/dogmell/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.yytec.dogmell.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 30/08/2017 22:38
@@ -19,7 +24,18 @@ public class IndexController {
     }
 
     @GetMapping("weather/now.json")
-    public String now() {
+    public String now(@PathVariable String lo) {
         return "{\"results\":[{\"location\":{\"id\":\"WS10730EM8EV\",\"name\":\"深圳\",\"country\":\"CN\",\"path\":\"深圳,深圳,广东,中国\",\"timezone\":\"Asia/Shanghai\",\"timezone_offset\":\"+08:00\"},\"now\":{\"text\":\"多云\",\"code\":\"4\",\"temperature\":\"31\"},\"last_update\":\"2017-09-01T17:20:00+08:00\"}]}";
+    }
+
+    @GetMapping("testservlet")
+    public Map<String, Object> test(HttpServletRequest request) {
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("url", request.getRequestURL());
+        ret.put("uri", request.getRequestURI());
+        ret.put("contextPath", request.getContextPath());
+        ret.put("servletPath", request.getServletPath());
+        ret.put("queryString", request.getQueryString());
+        return ret;
     }
 }
